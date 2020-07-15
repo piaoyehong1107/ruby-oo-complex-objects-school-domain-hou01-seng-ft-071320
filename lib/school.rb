cdf5a6cae799865e0d712c93cce1bf33e15860a0
Yehong Piao <piaoyehong1107@gmail.com>
2020-07-15 23:07:15 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,24 @@
-# code here!
+class School
+  
+def initialize(name,roster={})
+  @name=name
+  @roster=roster
+end
+
+def roster
+@roster
+end
+
+def add_student(name,grade)
+    keys = [ ]
+   @roster.each{|key,value|
+   keys.push(key)}
+    if keys.include?(grade)
+    @roster[grade].push(name)
+    else
+   @roster[grade] = []
+    @roster[grade] << name
+  end
+end
+
+end
